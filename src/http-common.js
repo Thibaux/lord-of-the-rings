819fb67fec58a966e43db6a90d169beb30f15e43
Thibaux Swildens <thibaux.swildens@gmail.com>
2020-12-27 15:02:02 +0100

    Changed location of api key

--- src/http-common.js
+++ src/http-common.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+let key = process.env.API_KEY;
+
 export const HTTP = axios.create({
-  baseURL: `https://the-one-api.dev/v2/`,
-  headers: {
-    Authorization: `Bearer ${process.env.API_KEY}`
-  }
+	baseURL: `https://the-one-api.dev/v2/`,
+	headers: {
+		Authorization: `Bearer ${key}`
+	}
 })
