6349c8c835aacf7d5b142a9f6a0286be3b8c1319
Thibaux Swildens <thibaux.swildens@gmail.com>
2021-01-01 14:35:00 +0100

    Temp api key fix

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,1 +1,5 @@
-module.exports = { NODE_ENV: '"production"', API_KEY: '"XI-qCVTiJ4bN8RBRanjN"' }  
+'use strict'
+module.exports = {
+	NODE_ENV: '"production"',
+	API_KEY: '"XI-qCVTiJ4bN8RBRanjN"'
+}
