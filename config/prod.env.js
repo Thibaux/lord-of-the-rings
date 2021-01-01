27a23d91aa6cc37334731c15e59edf6591de0f22
Thibaux Swildens <thibaux.swildens@gmail.com>
2021-01-01 13:25:32 +0100

    Fixed api key

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,4 +1,1 @@
-'use strict'
-module.exports = {
-	NODE_ENV: '"production"',
-}
+module.exports = { NODE_ENV: '"production"', API_KEY: '"XI-qCVTiJ4bN8RBRanjN"' }  
--- src/http-common.js
+++ src/http-common.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-let API_KEY = process.env.API_KEY
-
 export const HTTP = axios.create({
 	baseURL: `https://the-one-api.dev/v2/`,
 	headers: {
-		Authorization: `Bearer ${API_KEY}`
+		Authorization: `Bearer ${process.env.API_KEY}`
 	}
 })
